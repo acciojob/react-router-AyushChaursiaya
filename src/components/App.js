7b5e290fdd5b5a80a372899770881e4571723c22
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-08-10 15:11:30 +0000

    fileCode

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,53 @@
-import React from "react";
-import './../styles/App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./home";
-import About from "./about";
-import LayOut from "./layOut";
+import React from 'react'
+import { BrowserRouter, Link, Outlet, Route, Routes } from 'react-router-dom';
 
-const App = () => {
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my website!</h1>
+    </div>)
+}
+
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is a sample React Router program.</p>
+    </div>)
+}
+
+const LayOut = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='about'>About</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  )
+}
+
+
+
+
+function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='' element={<LayOut />} >
+          <Route path='' element={<LayOut />}>
             <Route path='/' element={<Home />} />
-            <Route path='/About' element={<About />} />
+            <Route path='/about' element={<About />} />
           </Route>
         </Routes>
       </BrowserRouter>
@@ -20,4 +55,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
