cc3e5836c43f8a6c5cd202a9f07a592f02b01e43
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-08-10 15:07:05 +0000

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home";
+import About from "./about";
+import LayOut from "./layOut";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='' element={<LayOut />} >
+            <Route path='/' element={<Home />} />
+            <Route path='/About' element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/layOut.js
@@ -0,0 +1,22 @@
+import { Link, Outlet } from "react-router-dom"
+
+const LayOut = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                </ul>
+            </nav>
+            <hr />
+            <Outlet />
+        </div>
+    )
+}
+
+export default LayOut;
